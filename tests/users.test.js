1642f53392952d4cb6ba3a7ddaba024d88477d21
SebSamPro <sebastien.samyn.pro@gmail.com>
2023-11-30 13:56:07 +0100

    add users tests

--- tests/users.test.js
+++ tests/users.test.js
@@ -2,8 +2,10 @@ const request = require("supertest");
 
 const app = require("../src/app");
 
-const database = require("../database")
-afterAll (() => database.end());
+const crypto = require("node:crypto");
+
+const database = require("../database");
+afterAll(() => database.end());
 
 describe("GET /api/users", () => {
   it("should return all users", async () => {
@@ -28,5 +30,55 @@ describe("GET /api/users/:id", () => {
     const response = await request(app).get("/api/users/0");
 
     expect(response.status).toEqual(404);
+  });
+});
+
+//create new user
+describe("POST /api/users", () => {
+  it("should return created users", async () => {
+    const newUser = {
+      firstname: "Tom",
+      lastname: "Cruise",
+      email: `${crypto.randomUUID()}@example.com`,
+      city: "Paris",
+      language: "English",
+    };
+
+    const response = await request(app).post("/api/users").send(newUser);
+
+    expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty("id");
+    expect(typeof response.body.id).toBe("number");
+
+    const [result] = await database.query(
+      "SELECT * FROM users WHERE id=?",
+      response.body.id
+    );
+
+    const [userInDatabase] = result;
+
+    expect(userInDatabase).toHaveProperty("id");
+
+    expect(userInDatabase).toHaveProperty("firstname");
+    expect(userInDatabase.firstname).toStrictEqual(newUser.firstname);
+
+    expect(userInDatabase).toHaveProperty("email");
+    expect(userInDatabase.email).toStrictEqual(newUser.email);
+
+    expect(userInDatabase).toHaveProperty("city");
+    expect(userInDatabase.city).toStrictEqual(newUser.city);
+
+    expect(userInDatabase).toHaveProperty("language");
+    expect(userInDatabase.language).toStrictEqual(newUser.language);
+  });
+
+  it("should return an error", async () => {
+    const movieWithMissingProps = { firstname: "Toto" };
+
+    const response = await request(app)
+      .post("/api/users")
+      .send(movieWithMissingProps);
+
+    expect(response.status).toEqual(500);
   });
 });
