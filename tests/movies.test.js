547e7c1c2094bff5abbe2c35af1d11895749c86b
SebSamPro <sebastien.samyn.pro@gmail.com>
2023-11-30 13:31:32 +0100

    soluce express 02_1

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -2,9 +2,10 @@ const request = require("supertest");
 
 const app = require("../src/app");
 
-const database = require("../database")
-afterAll (() => database.end());
+const database = require("../database");
+afterAll(() => database.end());
 
+//GET MOVIES
 describe("GET /api/movies", () => {
   it("should return all movies", async () => {
     const response = await request(app).get("/api/movies");
@@ -14,7 +15,7 @@ describe("GET /api/movies", () => {
     expect(response.status).toEqual(200);
   });
 });
-
+//GET MOVIE BY ID
 describe("GET /api/movies/:id", () => {
   it("should return one movie", async () => {
     const response = await request(app).get("/api/movies/1");
@@ -28,5 +29,58 @@ describe("GET /api/movies/:id", () => {
     const response = await request(app).get("/api/movies/0");
 
     expect(response.status).toEqual(404);
+  });
+});
+
+// test create movie
+describe("POST /api/movies", () => {
+  it("should return created movie", async () => {
+    const newMovie = {
+      title: "Star Wars",
+      director: "George Lucas",
+      year: "1977",
+      color: "1",
+      duration: 120,
+    };
+
+    const response = await request(app).post("/api/movies").send(newMovie);
+
+    expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty("id");
+    expect(typeof response.body.id).toBe("number");
+
+    const [result] = await database.query(
+      "SELECT * FROM movies WHERE id=?",
+      response.body.id
+    );
+
+    const [movieInDatabase] = result;
+
+    expect(movieInDatabase).toHaveProperty("id");
+
+    expect(movieInDatabase).toHaveProperty("title");
+    expect(movieInDatabase.title).toStrictEqual(newMovie.title);
+
+    expect(movieInDatabase).toHaveProperty("director");
+    expect(movieInDatabase.director).toStrictEqual(newMovie.director);
+
+    expect(movieInDatabase).toHaveProperty("year");
+    expect(movieInDatabase.year).toStrictEqual(newMovie.year);
+
+    expect(movieInDatabase).toHaveProperty("color");
+    expect(movieInDatabase.color).toStrictEqual(newMovie.color);
+
+    expect(movieInDatabase).toHaveProperty("duration");
+    expect(movieInDatabase.duration).toStrictEqual(newMovie.duration);
+  });
+
+  it("should return an error", async () => {
+    const movieWithMissingProps = { title: "Harry Potter" };
+
+    const response = await request(app)
+      .post("/api/movies")
+      .send(movieWithMissingProps);
+
+    expect(response.status).toEqual(500);
   });
 });
